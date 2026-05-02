55e81318cc2277000f9028b8b1b2940054dfc965
Vilém Makovička <makovickavilem@gmail.com>
2026-05-02 17:58:22 +0200

    cv update

---
+++ cv_project_sources/Warframe walkthrough/scripts/Menu-OnClick.js
@@ -0,0 +1,10 @@
+function Redirect(fv_url){
+    //var v_secs = 1;
+    //setInterval (function () {
+    //    v_secs--
+    //    if (v_secs == 0){
+    //        window.location = fv_url;
+    //    }
+    //}, 1000);
+    window.location = fv_url;
+}
